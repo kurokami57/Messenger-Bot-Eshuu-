c90b5d6f6e0ba3ee21f75e154376a409a44275dd
kurokami57 <nerob1277@gmail.com>
2025-11-13 14:53:42 +0600

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -1,62 +1,63 @@
 module.exports.config = {
-	name: "info",
-	version: "1.0.1", 
+	name: "inf",
+	version: "1.0.3",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-	description: "Admin and Bot info.",
-	commandCategory: "...",
+	credits: "Nerob",
+	description: "Aesthetic Admin & Bot Info",
+	commandCategory: "info",
 	cooldowns: 1,
-	dependencies: 
-	{
-    "request":"",
-    "fs-extra":"",
-    "axios":""
-  }
+	dependencies: {
+		"request": "",
+		"fs-extra": "",
+		"axios": ""
+	}
 };
-module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
-const axios = global.nodemodule["axios"];
-const request = global.nodemodule["request"];
-const fs = global.nodemodule["fs-extra"];
-const time = process.uptime(),
-		hours = Math.floor(time / (60 * 60)),
-		minutes = Math.floor((time % (60 * 60)) / 60),
-		seconds = Math.floor(time % 60);
-const moment = require("moment-timezone");
-var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
-var link =                                     
-["https://i.imgur.com/eDbdlvd.jpg"];
-var callback = () => api.sendMessage({body:` ╾━╤デ╦︻(▀̿Ĺ̯▀̿ ̿)🇮🇳 𝐀𝐃𝐌𝐈𝐍 𝐀𝐍𝐃 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 🇮🇳 
-(⌐▀͡ ̯ʖ▀)︻̷┻̿═━一-
 
-☄️Bot Name︎︎︎☄️  ${global.config.BOTNAME}
+module.exports.run = async function({ api, event }) {
+	const request = global.nodemodule["request"];
+	const fs = global.nodemodule["fs-extra"];
+	const moment = require("moment-timezone");
 
-🔥Bot Admin🔥☞︎︎︎☜︎︎︎✰ ℙ𝕣𝕚𝕪𝕒𝕟𝕤𝕙💔🥀
+	const time = process.uptime();
+	const hours = Math.floor(time / (60 * 60));
+	const minutes = Math.floor((time % (60 * 60)) / 60);
+	const seconds = Math.floor(time % 60);
+	const dateNow = moment.tz("Asia/Kolkata").format("『DD/MM/YYYY』 【HH:mm:ss】");
 
-🙈bot andmin owner facebook id link🙈➪ www.facebook.com/priyanshu.rajput.official 💞🕊️
+	// 🖼️ Use Facebook profile pic
+	const fbID = "61557548527867";
+	const fbPic = `https://graph.facebook.com/${fbID}/picture?width=720&height=720`;
 
-👋For Any Kind Of Help Contact On Telegram  Username 👉 @Priyanshrajput😇
-
-✧══════•❁❀❁•══════✧
-
-🌸Bot Prefix🌸☞︎︎︎☜︎︎︎✰ ${global.config.PREFIX}
-
-♥️Bot Owner♥️ ☞︎︎︎☜︎︎︎✰ 𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭
-
-🥳UPTIME🥳
-
-🌪️Today is🌪️ ☞︎︎︎☜︎︎︎✰ ${juswa} 
-
-⚡Bot is running⚡ ${hours}:${minutes}:${seconds}.
-
-✅Thanks for using ${global.config.BOTNAME} Bot🖤
+	const callback = () => api.sendMessage({
+		body: `✨ 𝗔𝗗𝗠𝗜𝗡 𝗔𝗡𝗗 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 ✨
 
 
-🦢🍒•••ꞪɛᏒɛ ɪʂ ɮ❍┼ ❍ωɳɜɽ ɳaʍɜ•••🌷💞
-┏━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┓
-🌸✦✧✧✧✧✰🍒ℙ𝕣𝕚𝕪𝕒𝕟𝕤𝕙🌿✰✧✧✧✧✦🌸
-┗━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┛
+⚜️ 𝗕𝗢𝗧 𝗡𝗔𝗠𝗘 ⚜️  
+➥ 𝗕𝗢𝗧: **${global.config.BOTNAME}**
 
+🔥 𝗢𝗪𝗡𝗘𝗥 🔥  
+➥ **𝗡𝗲𝗿𝗼𝗯**
 
-`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
-   };
+📞 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞 📞  
+➥ 𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: [𝗖𝗹𝗶𝗰𝗸 𝗛𝗲𝗿𝗲](https://www.facebook.com/${fbID})
+
+🌸 𝗕𝗢𝗧 𝗣𝗥𝗘𝗙𝗜𝗫 🌸  
+➥ **${global.config.PREFIX}**
+
+🕒 𝗧𝗜𝗠𝗘 & 𝗗𝗔𝗧𝗘 🕒  
+➥ ${dateNow}
+
+⚡ 𝗨𝗣𝗧𝗜𝗠𝗘 ⚡  
+➥ ${hours}𝗵 ${minutes}𝗺 ${seconds}𝘀
+
+💖 𝗧𝗵𝗮𝗻𝗸 𝘆𝗼𝘂 𝗳𝗼𝗿 𝘂𝘀𝗶𝗻𝗴 **${global.config.BOTNAME}** 💖
+
+✧══════•❁❀❁•══════✧`,
+		attachment: fs.createReadStream(__dirname + "/cache/nerob.jpg")
+	}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/nerob.jpg"));
+
+	// Download your FB profile picture
+	request(encodeURI(fbPic))
+		.pipe(fs.createWriteStream(__dirname + "/cache/nerob.jpg"))
+		.on("close", () => callback());
+};
